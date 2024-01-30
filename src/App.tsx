a5844d84c4f85ae3d42cf0124ac2eb4ba2972bc8
yaroslavzefi <yaroslavzef@gmail.com>
2024-01-30 09:23:48 +0300

    store

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {AppDispatch, fetchUsers, RootState} from "./store/store";
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
+    const dispatch = useDispatch<AppDispatch>();
+    const { users, loading, error } = useSelector((state: RootState) => state.users);
+
+    dispatch(fetchUsers());
+
+    if (loading === 'loading') {
+        return <div>Loading...</div>;
+    }
+
+    if (loading === 'failed') {
+        return <div>Error: {error}</div>;
+    }
+
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <h2>Users List</h2>
+            {users.map(({id, name, email}) => (
+                <div key={id}>
+                    <div>
+                        <span>name: </span>
+                        <strong>{name}</strong>
+                    </div>
+                   <div style={{marginBottom: '10px'}}>
+                       <span>email: </span>
+                       <strong> {email}</strong>
+                   </div>
+                </div>
+            ))}
+        </div>
     </div>
   );
 }
